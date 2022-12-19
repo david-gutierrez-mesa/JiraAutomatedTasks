1d624c76c6f8cf31936bdc4d31d755b6c036454d
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2022-12-19 15:37:52 +0100

    Modifying Headless tests according to the standarization

--- liferay/headless.py
+++ liferay/headless.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from helpers_jira import initialize_subtask_test_creation, initialize_subtask_test_validation, \
-    prepare_test_creation_subtask, prepare_test_validation_subtask, create_poshi_automation_task_for
+from helpers_jira import create_poshi_automation_task_for
 from jira_liferay import get_jira_connection
 
 
@@ -19,89 +18,99 @@ def _create_poshi_task_for(jira_local, parent_story, poshi_automation_table):
     print("Poshi task ", new_issue.key, " created for", parent_key)
 
 
-def create_creation_subtask(jira):
+def update_creation_subtask(jira):
     print("Creating test creation subtasks for Headless team...")
-    stories_without_testing_subtask = jira.search_issues('filter=54996')
-    for story in stories_without_testing_subtask:
-        print("Creating sub-tasks for story " + story.key)
-        test_creation, components = prepare_test_creation_subtask(story)
+    stories_with_test_creation_subtask = jira.search_issues('filter=54996')
+    for story in stories_with_test_creation_subtask:
+        for subtask in story.fields.subtasks:
+            summary = subtask.fields.summary
+            key = subtask.key
+            test_creation_subtask_details = jira.search_issues("key =" + key)
+            assignee = test_creation_subtask_details[0].fields.assignee
+            if summary == 'Test Scenarios Coverage | Test Creation':
+                print("Updating "+key+" ...")
+                if subtask.fields.status.name == "Open":
+                    description = '*Output*' \
+                                  '\r\n # Our table with the Test scenarios/test cases to be validated in the ' \
+                                  'validation phase.' \
+                                  '\r\n # After being reviewed by the team, add a finalized table to the parent story ' \
+                                  'description' \
+                                  '\r\n # Add test cases to [Test ' \
+                                  'Map|https://docs.google.com/spreadsheets/d/19KSqxtKJQ5FHZbHKxDS3_TzptWeD0DrL-mLk0y0WFYY' \
+                                  '/edit#gid=2145200593]' \
+                                  '\r\n' \
+                                  '\r\n*Test Scenarios:*' \
+                                  '\r\n||Requirement||Test Case||Covered by unit/integration test? (Yes/No)||Test Priority (' \
+                                  'business impact)||' \
+                                  '\r\n| | | | |' \
+                                  '\r\n' \
+                                  '\r\n*Exploratory testing to consider:*' \
+                                  '\r\n||Requirement||Test Scenarios||Test Priority (business impact)||Covered by ' \
+                                  'frontend/backend Unit Test?||' \
+                                  '\r\n| | | | |'
+                    subtask.update(fields={'description': description})
+                    if assignee != 'Support QA':
+                        jira.assign_issue(subtask.id, 'support-qa')
+                        break
+                    break
+    print("Subtasks Test Creation for Headless team are up to date")
 
-        if test_creation:
-            description = '*Output*' \
-                          '\r\n # Our table with the Test scenarios/test cases to be validated in the ' \
-                          'validation phase.' \
-                          '\r\n # After being reviewed by the team, add a finalized table to the parent story ' \
-                          'description' \
-                          '\r\n # Add test cases to [Test ' \
-                          'Map|https://docs.google.com/spreadsheets/d/19KSqxtKJQ5FHZbHKxDS3_TzptWeD0DrL-mLk0y0WFYY' \
-                          '/edit#gid=2145200593]' \
-                          '\r\n' \
-                          '\r\n*Test Scenarios:*' \
-                          '\r\n||Requirement||Test Case||Covered by unit/integration test? (Yes/No)||Test Priority (' \
-                          'business impact)||' \
-                          '\r\n| | | | |' \
-                          '\r\n' \
-                          '\r\n*Exploratory testing to consider:*' \
-                          '\r\n||Requirement||Test Scenarios||Test Priority (business impact)||Covered by ' \
-                          'frontend/backend Unit Test?||' \
-                          '\r\n| | | | |'
-            subtask_test_creation = initialize_subtask_test_creation(story, components, description)
-            child = jira.create_issue(fields=subtask_test_creation)
-            print("* Created sub-task: " + child.key)
 
-    print("Subtasks for Headless team are up to date")
-
-
-def create_validation_subtask(jira):
-    print("Creating test validation subtasks for Headless team...")
-    stories_without_testing_subtask = jira.search_issues('filter=54997')
-    for story in stories_without_testing_subtask:
-        print("Creating sub-tasks for story " + story.key)
-        test_validation, components = prepare_test_validation_subtask(story)
-
-        if test_validation:
-            description = '\r\n*Context*' \
-                          '\r\nExecute the tests of the parent story, and use the information in the*Test ' \
-                          'Information*section to perform the tests.' \
-                          '\r\n' \
-                          '\r\n*Output*' \
-                          '\r\nTell in one comment (in the story ticket) the final status of this first round, ' \
-                          'and in this ticket, fill the bug section.' \
-                          '\r\nRemember to link the bug (if you discover it) with the Story ticket.' \
-                          '\r\n{code:java}' \
-                          '\r\n*{color:#14892c}PASSED{color}* / *{color:#d04437}FAILED{color}* / *{' \
-                          'color:#59afe1}BLOCKED{color}* Manual Testing following the steps in the description.' \
-                          '\r\n' \
-                          '\r\n*Verified on:*' \
-                          '\r\n*Environment*: localhost' \
-                          '\r\n*Github*: https://github.com/liferay/liferay-portal.git' \
-                          '\r\n*Branch*: master' \
-                          '\r\n*Bundle*: Liferay DXP' \
-                          '\r\n*Database*: MySQL 5.7.22' \
-                          '\r\n*Last Commit*: ? ' \
-                          '\r\n' \
-                          '\r\n|| Test Scenarios || Test Result ||' \
-                          '\r\n| |*{color:#14892c}PASSED{color}* / *{color:#d04437}FAILED{color}* / *{' \
-                          'color:#59afe1}BLOCKED{color}*|' \
-                          '\r\n| |*{color:#14892c}PASSED{color}* / *{color:#d04437}FAILED{color}* / *{' \
-                          'color:#59afe1}BLOCKED{color}*|' \
-                          '\r\n...' \
-                          '\r\n{code}' \
-                          '\r\n*Bugs:*' \
-                          '\r\n (/)- PASS' \
-                          '\r\n (!)- To Do' \
-                          '\r\n (x)- FAIL' \
-                          '\r\n * *Impeditive:*' \
-                          '\r\n||Ticket||Title||' \
-                          '\r\n|?|?|' \
-                          '\r\n * *Not Impeditive:*' \
-                          '\r\n||Ticket||Title||' \
-                          '\r\n|?|?|'
-            subtask_test_validation = initialize_subtask_test_validation(story, components, description)
-            child = jira.create_issue(fields=subtask_test_validation)
-            print("* Created sub-task: " + child.key)
-
-    print("Subtasks for Headless team are up to date")
+def update_validation_subtask(jira):
+    print("Updating test validation subtasks for Headless team...")
+    stories_with_test_validation_subtask = jira.search_issues('filter=55398')
+    for story in stories_with_test_validation_subtask:
+        for subtask in story.fields.subtasks:
+            summary = subtask.fields.summary
+            key = subtask.key
+            test_validation_subtask_details = jira.search_issues("key =" + key)
+            assignee = test_validation_subtask_details[0].fields.assignee
+            if summary == 'Product QA | Test Validation - Round 1':
+                print("Updating "+key+" ...")
+                if subtask.fields.status.name == "Open":
+                    description = '\r\n*Context*' \
+                                  '\r\nExecute the tests of the parent story, and use the information in the *Test ' \
+                                  'Information* section to perform the tests.' \
+                                  '\r\n' \
+                                  '\r\n*Output*' \
+                                  '\r\nTell in one comment (in the story ticket) the final status of this first round, ' \
+                                  'and in this ticket, fill the bug section.' \
+                                  '\r\nRemember to link the bug (if you discover it) with the Story ticket.' \
+                                  '\r\n{code:java}' \
+                                  '\r\n*{color:#14892c}PASSED{color}* / *{color:#d04437}FAILED{color}* / *{' \
+                                  'color:#59afe1}BLOCKED{color}* Manual Testing following the steps in the description.' \
+                                  '\r\n' \
+                                  '\r\n*Verified on:*' \
+                                  '\r\n*Environment*: localhost' \
+                                  '\r\n*Github*: https://github.com/liferay/liferay-portal.git' \
+                                  '\r\n*Branch*: master' \
+                                  '\r\n*Bundle*: Liferay DXP' \
+                                  '\r\n*Database*: MySQL 5.7.22' \
+                                  '\r\n*Last Commit*: ? ' \
+                                  '\r\n' \
+                                  '\r\n|| Test Scenarios || Test Result ||' \
+                                  '\r\n| |*{color:#14892c}PASSED{color}* / *{color:#d04437}FAILED{color}* / *{' \
+                                  'color:#59afe1}BLOCKED{color}*|' \
+                                  '\r\n| |*{color:#14892c}PASSED{color}* / *{color:#d04437}FAILED{color}* / *{' \
+                                  'color:#59afe1}BLOCKED{color}*|' \
+                                  '\r\n...' \
+                                  '\r\n{code}' \
+                                  '\r\n*Bugs:*' \
+                                  '\r\n (/)- PASS' \
+                                  '\r\n (!)- To Do' \
+                                  '\r\n (x)- FAIL' \
+                                  '\r\n * *Impeditive:*' \
+                                  '\r\n||Ticket||Title||' \
+                                  '\r\n|?|?|' \
+                                  '\r\n * *Not Impeditive:*' \
+                                  '\r\n||Ticket||Title||' \
+                                  '\r\n|?|?|'
+                    subtask.update(fields={'description': description})
+                    if assignee != 'Support QA':
+                        jira.assign_issue(subtask.id, 'support-qa')
+                        break
+                    break
+    print("Validation subtasks for Headless team are up to date")
 
 
 def create_poshi_automation_task(jira):
@@ -124,6 +133,6 @@ def create_poshi_automation_task(jira):
 
 if __name__ == "__main__":
     jira_connection = get_jira_connection()
-    create_creation_subtask(jira_connection)
-    create_validation_subtask(jira_connection)
+    update_creation_subtask(jira_connection)
+    update_validation_subtask(jira_connection)
     create_poshi_automation_task(jira_connection)
