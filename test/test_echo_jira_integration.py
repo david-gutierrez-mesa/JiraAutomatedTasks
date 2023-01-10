f40812b17b38e397c615862318c99767fbbe231c
David Gutierrez <david.gutierrez@liferay.com>
2023-01-10 12:54:28 +0100

    JAT-0019 Add tests for assign_qa_engineer method

--- test/test_echo_jira_integration.py
+++ test/test_echo_jira_integration.py
@@ -1,29 +1,36 @@
 import unittest
 
-from liferay.echo import create_poshi_automation_task, transition_story_to_ready_for_pm_review, \
-    create_poshi_automation_task_for_bugs
+from liferay.echo import *
 from liferay.jira_liferay import get_jira_connection
 
 
 class EchoJiraTestCase(unittest.TestCase):
-    def test_create_poshi_automation_task(self):
-        jira_connection = get_jira_connection()
+
+    def test_assign_qa_engineer(self):
+        jira_connection_test = get_jira_connection()
         try:
-            create_poshi_automation_task(jira_connection)
+            assign_qa_engineer(jira_connection_test, '')
+        except Exception:
+            self.fail("Test failed unexpectedly!")
+
+    def test_create_poshi_automation_task(self):
+        jira_connection_test = get_jira_connection()
+        try:
+            create_poshi_automation_task(jira_connection_test, '', '')
         except Exception:
             self.fail("Test failed unexpectedly!")
 
     def test_create_poshi_automation_task_for_bugs(self):
-        jira_connection = get_jira_connection()
+        jira_connection_test = get_jira_connection()
         try:
-            create_poshi_automation_task_for_bugs(jira_connection)
+            create_poshi_automation_task_for_bugs(jira_connection_test, '')
         except Exception:
             self.fail("Test failed unexpectedly!")
 
     def test_transition_story_to_ready_for_pm_review(self):
-        jira_connection = get_jira_connection()
+        jira_connection_test = get_jira_connection()
         try:
-            transition_story_to_ready_for_pm_review(jira_connection)
+            transition_story_to_ready_for_pm_review(jira_connection_test, '', '')
         except Exception:
             self.fail("Test failed unexpectedly!")
 
