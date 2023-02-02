72aaa55a85fc6844bd642991de37a728bb3ac68c
David Gutierrez <david.gutierrez@liferay.com>
2023-02-02 10:19:54 +0100

    JAT-0021 Add test for check_bug_threshold

--- test/test_echo_test_map.py
+++ test/test_echo_test_map.py
@@ -1,6 +1,7 @@
 import unittest
 
-from echo_test_map import add_test_cases_to_test_map, check_need_automation_test_cases, check_control_panel_tab
+from echo_test_map import add_test_cases_to_test_map, check_need_automation_test_cases, check_control_panel_tab, \
+    check_bug_threshold
 from jira_liferay import get_jira_connection
 from testmap_jira import get_testmap_connection
 
@@ -13,6 +14,16 @@ class EchoTestMapTests(unittest.TestCase):
             jira_connection = get_jira_connection()
             sheet_connection = get_testmap_connection()
             message, info = add_test_cases_to_test_map(sheet_connection, jira_connection, message, info)
+        except Exception:
+            self.fail("Test failed unexpectedly!")
+
+    def test_check_bug_threshold(self):
+        try:
+            message = ''
+            info = ''
+            jira_connection = get_jira_connection()
+            sheet_connection = get_testmap_connection()
+            message, info = check_bug_threshold(sheet_connection, jira_connection, message, info)
         except Exception:
             self.fail("Test failed unexpectedly!")
 
