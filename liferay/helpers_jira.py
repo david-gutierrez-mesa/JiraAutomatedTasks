e02bb9d7aa96daf36f74c5530963cc2fd2edfaa2
David Gutierrez <david.gutierrez@liferay.com>
2023-03-14 13:35:34 +0100

    JAT-0026 Fix error when table was not followed by Test Case title

--- liferay/helpers_jira.py
+++ liferay/helpers_jira.py
@@ -159,9 +159,12 @@ def prepare_test_validation_subtask(story):
 def read_test_cases_table_from_description(description):
     table_starring_string = '||Test Scenarios||'
     table_ending_string = 'h3. Test Cases'
+    table_alternative_ending_string = '*Case '
     table_staring_position = description.find(table_starring_string)
     table_ending_position = description.find(table_ending_string)
     if table_staring_position != -1:
+        if table_ending_position == -1:
+            table_ending_position = description.find(table_alternative_ending_string)
         table = description[table_staring_position:table_ending_position]
         table_rows = table.split('\r\n')
         table_rows = [value for value in table_rows if value != '']
