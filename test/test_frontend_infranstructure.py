c7963171bfe3a48dae0f0b89640ad214d1bcfa38
David Gutierrez <david.gutierrez@liferay.com>
2023-05-17 13:46:00 +0200

    JAT-0034 Add test for new method

---
+++ test/test_frontend_infranstructure.py
@@ -0,0 +1,19 @@
+import unittest
+
+from frontend_infrastructure import *
+from jira_liferay import get_jira_connection
+
+
+class EchoTestMapTests(unittest.TestCase):
+
+    def test_create_technical_sub_task_test_scope_out_of_scope_creation(self):
+        try:
+            info_test = ''
+            jira_connection_test = get_jira_connection()
+            info_test = create_technical_sub_task_test_scope_out_of_scope_creation(jira_connection_test, info_test)
+        except Exception:
+            self.fail("Test failed unexpectedly!")
+
+
+if __name__ == '__main__':
+    unittest.main()
