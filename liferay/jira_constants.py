bca16f3afed64859d1a02696d27546be10d956f6
David Gutierrez <david.gutierrez@liferay.com>
2023-05-18 17:41:48 +0200

    JAT-0035 Reorder filters

--- liferay/jira_constants.py
+++ liferay/jira_constants.py
@@ -5,26 +5,24 @@ class CustomField:
 
 
 class Filter:
-    Assign_QA_Engineer = 'filter=54607'
     All_bugs_in_Ready_for_Release = 'filter=54632'
-    Integration_Sub_task_creation = 'filter=54572'
-    Ready_to_create_test_table_on_description = 'filter=54772'
-    Ready_to_create_POSHI_automation_task = 'filter=54646'
+    Assign_QA_Engineer = 'filter=54607'
     Closed_Bugs_with_FP4_and_FP5_without_automation_task = 'filter=51790'
+    Echo_7_4_CE_GA_All = 'filter=49998'
+    FI_Ready_for_Testing_Stories_without_Test_Validation = 'filter=55093'
+    FI_Ready_to_Create_poshi_tasks = 'filter=55095'
+    FI_Stories_without_test_creation_subtask = 'filter=55092'
+    FI_Test_Scope_out_of_Scope_Creation_task_creation = 'filter=56504'
+    GSheets_FI_7_4_CE_GA_All = 'filter=50189'
+    Headless_Team_Ready_to_create_POSHI_Automation_Task = 'filter=54999'
+    Integration_In_Development_Sub_task_creation_Headless_team = 'filter=54996'
+    Integration_Sub_task_creation = 'filter=54572'
+    Product_QA_Test_Validation_Round_1 = 'filter=55398'
+    Ready_to_create_POSHI_automation_task = 'filter=54646'
+    Ready_to_create_test_table_on_description = 'filter=54772'
     Round_tasks_without_description = 'filter=56455'
     Stories_ready_to_be_closed = 'filter=55152'
     Stories_to_add_into_test_map = 'filter=55104'
-    Echo_7_4_CE_GA_All = 'filter=49998'
-    GSheets_FI_7_4_CE_GA_All = 'filter=50189'
-    FI_Stories_without_test_creation_subtask = 'filter=55092'
-    FI_Ready_for_Testing_Stories_without_Test_Validation = 'filter=55093'
-    FI_Ready_to_Create_poshi_tasks = 'filter=55095'
-    FI_Test_Scope_out_of_Scope_Creation_task_creation = 'filter=56504'
-    Integration_In_Development_Sub_task_creation_Headless_team = 'filter=54996'
-    Product_QA_Test_Validation_Round_1 = 'filter=55398'
-    Headless_Team_Ready_to_create_POSHI_Automation_Task = 'filter=54999'
-
-
 
 
 class Status:
