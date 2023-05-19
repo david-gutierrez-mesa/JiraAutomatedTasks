238a3a26a561b60cbb940c32f2ea2a4b40b6802f
David Gutierrez <david.gutierrez@liferay.com>
2023-05-19 12:38:55 +0200

    fiChanges - Add test for update_frontend_infra_bug_threshold

--- test/test_frontend_infra_test_map.py
+++ test/test_frontend_infra_test_map.py
@@ -16,6 +16,15 @@ class EchoTestMapTests(unittest.TestCase):
         except Exception:
             self.fail("Test failed unexpectedly!")
 
+    def test_update_frontend_infra_bug_threshold(self):
+        try:
+            info_test = ''
+            jira_connection_test = get_jira_connection()
+            sheet_connection_test = get_testmap_connection()
+            info_test = update_frontend_infra_bug_threshold(sheet_connection_test, jira_connection_test, info_test)
+        except Exception:
+            self.fail("Test failed unexpectedly!")
+
 
 if __name__ == '__main__':
     unittest.main()
