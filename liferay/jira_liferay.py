8b9ce30bfa606ae7a5afecf26b85eafb1cbc29da
David Gutierrez <david.gutierrez@liferay.com>
2023-05-22 17:37:33 +0200

    JAT-0035 Adapt to use jira_constants

--- liferay/jira_liferay.py
+++ liferay/jira_liferay.py
@@ -1,9 +1,21 @@
 from jira import JIRA
 
+from jira_constants import Instance
 from manageCredentialsCrypto import get_credentials
 
 
 def get_jira_connection():
     login = get_credentials()
-    jira = JIRA("https://issues.liferay.com", basic_auth=login)
+    try:
+        if Instance.Type == "Cloud":
+            jira = JIRA(Instance.Jira_URL, basic_auth=login)
+        elif Instance.Type == "Server":
+            jira = JIRA(Instance.Jira_URL, token_auth=login[1])
+        else:
+            raise Exception("Sorry, only Cloud and Server are valid values for Instance.Type")
+    except Exception as err:
+        raise Exception("Error accessing Jira instance. Please check .jira_user folder and jira_constants.py: "
+                        "Error message\n" +
+                        str(err))
+
     return jira
