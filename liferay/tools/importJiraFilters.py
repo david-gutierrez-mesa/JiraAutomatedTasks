4f9e2aaf8d86a3bc210daf4dd03bda8baca2b8be
David Gutierrez <david.gutierrez@liferay.com>
2023-05-31 13:12:30 +0200

    JAT-0040 Update import Jira Filters

--- liferay/tools/importJiraFilters.py
+++ liferay/tools/importJiraFilters.py
@@ -5,10 +5,17 @@ import pickle
 from jira import JIRAError
 
 from jira_liferay import get_jira_connection
+from manageCredentialsCrypto import delete_credentials
+from utils.jira_helpers import set_filter_permissions
 
 
 def main():
-    jira_connection = get_jira_connection()
+    print("Please, enter the URL of Jira instance where you want to import your filters\n")
+    # jira_url = input("Jira URL: ")
+    jira_url = "https://liferay-sandbox-822.atlassian.net"
+    jira_type = "Cloud"
+    # delete_credentials()
+    jira_connection = get_jira_connection(jira_url, jira_type)
     user_id = jira_connection.user(jira_connection.current_user()).emailAddress.split('@')[0]
     file_name = user_id + '_filters.pkl'
     with open(file_name, 'rb') as inp:
@@ -17,6 +24,7 @@ def main():
     error = 0
     imported = 0
     skipped = 0
+    filters_to_import.sort(key=lambda x: x.id, reverse=False)
 
     for filter_to_import in filters_to_import:
         if any(obj.name == filter_to_import.name for obj in existing_filters):
@@ -27,10 +35,13 @@ def main():
                 description = ''
                 if hasattr(filter_to_import, 'property'):
                     description = filter_to_import.description
-                jira_connection.create_filter(name=filter_to_import.name, description=description,
-                                              jql=filter_to_import.jql, favourite=filter_to_import.favourite)
+                new_filter = jira_connection.create_filter(name=filter_to_import.name, description=description,
+                                                           jql=filter_to_import.jql,
+                                                           favourite=filter_to_import.favourite)
                 print(
-                    '[IMPORTED] Filter "' + filter_to_import.name + '" did NOT exists in destination asn was imported')
+                    '[IMPORTED] Filter "' + filter_to_import.name + '" did NOT exists in destination and was imported')
+                for permission in filter_to_import.sharePermissions:
+                    set_filter_permissions(jira_connection, jira_url, new_filter.id, permission)
                 imported += 1
             except JIRAError as err:
                 print('[ERROR] Filter "' + filter_to_import.name +
