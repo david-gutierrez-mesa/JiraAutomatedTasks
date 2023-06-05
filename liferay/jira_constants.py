37b93d8b3f90066c54b97c1c0df485f53e5480b6
David Gutierrez <david.gutierrez@liferay.com>
2023-06-05 12:53:06 +0200

    JAT-0043 Add new filter

--- liferay/jira_constants.py
+++ liferay/jira_constants.py
@@ -9,6 +9,7 @@ class Filter:
     Assign_QA_Engineer = 'filter=54607'
     Closed_Bugs_with_FP4_and_FP5_without_automation_task = 'filter=51790'
     Echo_7_4_CE_GA_All = 'filter=49998'
+    Echo_bug_threshold = 'filter=56598'
     FI_ALL_bugs_unresolved_Manoel = 'filter=56141'
     FI_Ready_for_Testing_Stories_without_Test_Validation = 'filter=55093'
     FI_Ready_to_Create_poshi_tasks = 'filter=55095'
