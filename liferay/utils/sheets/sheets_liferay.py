f3384c48c75b2e096018448c3e00933f608f472e
David Gutierrez <david.gutierrez@liferay.com>
2023-06-14 13:56:54 +0200

    JAT-0048 Create a package for sheets files

--- liferay/utils/sheets/sheets_liferay.py
+++ liferay/utils/sheets/sheets_liferay.py
@@ -9,23 +9,23 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+from liferay.utils.sheets.sheets_constants import SheetInstance
 
 
 def get_testmap_connection():
     home = Path(expanduser("~"))
     folder = home / ".testmap_user"
     credentials_file = folder / "credentials.json"
-    token_file = "../token.json"
+    token_file = "../../token.json"
     creds = None
-    if os.path.exists('../token.json'):
-        creds = Credentials.from_authorized_user_file(abspath(token_file), SCOPES)
+    if os.path.exists('../../token.json'):
+        creds = Credentials.from_authorized_user_file(abspath(token_file), SheetInstance.SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                abspath(credentials_file), SCOPES)
+                abspath(credentials_file), SheetInstance.SCOPES)
             creds = flow.run_local_server(port=0)
         with open(token_file, 'w') as token:
             token.write(creds.to_json())
