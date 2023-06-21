4c446e60cf8a2a3d6b22ccbbef044f6a8bea9475
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2023-06-21 14:20:16 +0200

    Update Headless

---
+++ liferay/jira_constants.py
@@ -0,0 +1,42 @@
+class CustomField:
+    Epic_Link = 'customfield_12821'
+    Fix_Priority = 'customfield_12523'
+    QA_Engineer = 'customfield_24852'
+
+
+class Filter:
+    All_bugs_in_Ready_for_Release = 'filter=54632'
+    Assign_QA_Engineer = 'filter=54607'
+    Closed_Bugs_with_FP4_and_FP5_without_automation_task = 'filter=51790'
+    Echo_7_4_CE_GA_All = 'filter=49998'
+    FI_ALL_bugs_unresolved_Manoel = 'filter=56141'
+    FI_Ready_for_Testing_Stories_without_Test_Validation = 'filter=55093'
+    FI_Ready_to_Create_poshi_tasks = 'filter=55095'
+    FI_Stories_without_test_creation_subtask = 'filter=55092'
+    FI_Test_Scope_out_of_Scope_Creation_task_creation = 'filter=56504'
+    GSheets_FI_7_4_CE_GA_All = 'filter=50189'
+    GSheets_Headless_All_Stories = 'filter=11507'
+    Headless_All_verified_Bugs_in_master = 'filter=54623'
+    Headless_Team_Ready_to_create_POSHI_Automation_Task = 'filter=12225'
+    Integration_In_Development_Sub_task_creation_Headless_team = 'filter=12231'
+    Integration_Sub_task_creation = 'filter=54572'
+    Product_QA_Test_Validation_Round_1 = 'filter=12232'
+    Ready_to_create_POSHI_automation_task = 'filter=54646'
+    Ready_to_create_test_table_on_description = 'filter=54772'
+    Round_tasks_without_description = 'filter=56455'
+    Stories_ready_to_be_closed = 'filter=55152'
+    Stories_to_add_into_test_map = 'filter=55104'
+
+
+class Instance:
+    Jira_URL = "https://issues.liferay.com"
+    Type = "Server"
+
+
+class Status:
+    Closed = 'Closed'
+    In_Testing = 'In Testing'
+    Open = 'Open'
+    Ready_for_Product_Review = 'Ready for Product Review'
+    Ready_for_testing = '10619'
+    Selected_for_development = 'Selected for Development'
