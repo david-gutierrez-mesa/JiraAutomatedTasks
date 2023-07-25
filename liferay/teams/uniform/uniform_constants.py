70a2dbf5dabf903b66764bc5a93c520d26c81e22
David Gutierrez <david.gutierrez@liferay.com>
2023-07-25 08:48:49 +0200

    LPS-190913 Remove not needed constants

--- liferay/teams/uniform/uniform_constants.py
+++ liferay/teams/uniform/uniform_constants.py
@@ -1,8 +1,7 @@
 class FileName:
-    OUTPUT_BUG_THRESHOLD_EXCEED_FILE_NAME = "bug_threshold_exceed_message_uniform.txt"
-    OUTPUT_BUG_THRESHOLD_WARNING_FILE_NAME = "bug_threshold_warning_message_uniform.txt"
     OUTPUT_INFO_FILE_NAME = "output_info_uniform.txt"
     OUTPUT_MESSAGE_FILE_NAME = "output_message_uniform.txt"
+
 
 class Sheets:
     UNIFORM_TESTMAP_ID = '1iucCSBLgzdJ-B4ZoDUWV_UwHdOgFSh8z5p6TjbxQ6i0'
