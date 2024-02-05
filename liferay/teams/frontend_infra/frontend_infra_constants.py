2a13438f609d6bca35af5a1f43d52fce4bdb9204
david-gutierrez-mesa <david.gutierrez@liferay.com>
2024-02-05 13:37:00 +0100

    JAT-0099 Move FI constants inside FI folder

--- liferay/teams/frontend_infra/frontend_infra_constants.py
+++ liferay/teams/frontend_infra/frontend_infra_constants.py
@@ -4,6 +4,14 @@ class FileName:
     OUTPUT_BUG_THRESHOLD_INFO_FILE_NAME = "bug_threshold_output_info_fi.txt"
 
 
+class Filter:
+    FI_ALL_bugs_unresolved_Manoel = 'filter=11516'
+    FI_Ready_for_Testing_Stories_without_Test_Validation = 'filter=12425'
+    FI_Stories_without_test_creation_subtask = 'filter=12424'
+    FI_Test_Scope_out_of_Scope_Creation_task_creation = 'filter=12855'
+    GSheets_FI_7_4_CE_GA_All = 'filter=13335'
+
+
 class Roles:
     QA_JIRA_USER = 'carlos.brichete'
 
