dbc7f95527929594c24d88a5ae3295196790034f
david-gutierrez-mesa <david.gutierrez@liferay.com>
2024-02-05 13:37:22 +0100

    JAT-0099 Move Peds constants inside Headless Peds

--- liferay/teams/peds/peds_constants.py
+++ liferay/teams/peds/peds_constants.py
@@ -3,5 +3,10 @@ class FileName:
     OUTPUT_MESSAGE_FILE_NAME = "output_message_peds.txt"
 
 
+class Filter:
+    Peds_Bugs_Unresolved = 'filter=12726'
+    Peds_7_4_CE_GA_All = 'filter=13148'
+
+
 class Sheets:
     PEDS_TESTMAP_ID = '1AIummEUHEBLbAW-SWYDj_pcEBVpoyh_99_mDT9CZF6Q'
